7534c6b14fb4b268613d6975116bcbf7a06b450c
Ayo <Ayo.meshe@gmail.com>
2025-04-10 18:34:30 +0100

    app test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
 import Image from "next/image";
 
 export default function Home() {
+  const dbUrl = process.env.DATABASE_URL;
+
   return (
-    <div className="flex items-center justify-center">
-      <h1 className="text-3xl bg-red-600">
-        This my first official landing page woohoo!
+    <div className="flex flex-col items-center justify-center h-screen space-y-4 bg-gray-900">
+      <h1 className="text-3xl text-white bg-red-600">
+        This my first official landing page that is centred in the middle
+        woohoo!
       </h1>
+      <h3 className="text-3xl text-white bg-green-600">{dbUrl}</h3>
     </div>
   );
 }
