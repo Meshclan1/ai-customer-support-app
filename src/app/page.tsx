d033a5106325b51fb74e3d7bb90a62ef96fddccb
Ayo <Ayo.meshe@gmail.com>
2025-04-10 18:05:42 +0100

    Initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex items-center justify-center">
+      <h1 className="text-3xl bg-red-600">
+        This my first official landing page woohoo!
+      </h1>
+    </div>
+  );
+}
